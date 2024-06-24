1fc6e38cd981b84180de1892cfce6540f29f645b
SarahRosniani <sarahrosniani679@gmail.com>
2024-06-24 09:56:46 +0700

    db

--- database/db.js
+++ database/db.js
@@ -1,32 +1,29 @@
-import mysql from 'mysql2';
+import pg from 'pg';
 import dotenv from 'dotenv';
 
+const { Pool } = pg;
 
 dotenv.config();
 
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL,
 });
 
-db.connect((err) => {
-    if (err) {
-        console.error('Database connection failed:', err.stack);
-        return;
-    }
-    console.log('Connected to database.');
+pool.connect((err) => {
+  if (err) {
+    console.error('Database connection failed:', err.stack);
+    return;
+  }
+  console.log('Connected to database.');
 });
 
-export const testConnection = () => {
-    db.ping((err) => {
-        if (err) {
-            console.error('Database ping failed:', err.message);
-        } else {
-            console.log('Database connection is alive.');
-        }
-    });
+export const testConnection = async () => {
+  try {
+    const res = await pool.query('SELECT NOW()');
+    console.log('Database connection is alive:', res.rows[0]);
+  } catch (err) {
+    console.error('Database ping failed:', err.message);
+  }
 };
 
-export default db;
+export default pool;
